b3758b64e8d707f6490d061687bf215478f4e891
yunus103 <yunusemre.aytekin@ogr.iuc.edu.tr>
2025-08-11 21:17:59 +0300

    JS functions declared

--- script.js
+++ script.js
@@ -10,17 +10,28 @@ const twoMonths =  document.querySelectorAll(".two-months");
 const nextButton = document.querySelectorAll('#next');
 const backButton = document.querySelectorAll('#back');
 
+let payPeriod = 1; // 1 for monthly, 2 for yearly
+let plan = 1; // 1 for Arcade, 2 for Advanced, 3 for pro
+
+let totalPrice = 9;
+
 nextButton.forEach(button => {
     button.addEventListener('click', () => {
         const currentPage = document.querySelector('.section.active');
         let currentPageIndex = Number(currentPage.id.slice(-1));
-        currentPageIndex += 1;
+        console.log(currentPageIndex);
 
+        if(currentPageIndex === 2){
+            setAddOns();
+        }
+
+        currentPageIndex += 1;
         currentPage.classList.remove('active');
         const nextPage = document.getElementById('step-' + currentPageIndex);
         nextPage.classList.add('active');
     });
 });
+
 
 backButton.forEach(button => {
     button.addEventListener('click', () => {
@@ -44,6 +55,8 @@ toggle.addEventListener('change', () => {
         advancedDetail.textContent = "120/yr";
         proDetail.textContent = "150/yr";
 
+        payPeriod = 2;
+
         twoMonths.forEach(element => {
             element.classList.add("active");
         });    
@@ -55,8 +68,49 @@ toggle.addEventListener('change', () => {
         advancedDetail.textContent = "12/mo";
         proDetail.textContent = "15/mo";
 
+        payPeriod = 1;
+
         twoMonths.forEach(element => {
             element.classList.remove("active");
         });  
     }
 });
+
+function getPlan() {
+    const selectedPlan = document.querySelector('input[name="plan"]:checked');
+
+    if(selectedPlan)
+    {
+        if(selectedPlan.value === "arcade")
+        {
+            plan = 1;
+        } else if(selectedPlan.value === "advanced") {
+            plan = 2;
+        } else if(selectedPlan.value === "pro"){
+            plan = 3;
+        }
+    }else
+        return -1;
+}
+
+function setAddOns() {
+    const yearlyAddOn1 = document.getElementById('add-1');
+    const yearlyAddOn2 = document.querySelectorAll('#add-2');
+    
+    if(payPeriod === 2){    
+        yearlyAddOn1.textContent = "+$10/yr";
+        yearlyAddOn2.forEach(element => {
+            element.textContent = "+$20/yr";
+        });
+    } else if(payPeriod === 1) {
+        yearlyAddOn1.textContent = "+$1/mo";
+        yearlyAddOn2.forEach(element => {
+            element.textContent = "+$2/mo";
+        });
+    }
+}
+
+function calculateAddOns() {
+    const selectedAddOns = document.qu
+}
+
